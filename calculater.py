954157c9d406220f16f88f64875f41c0bb60a3a3
Sakshi Rajput <ranjupsakshig@gamil.com>
2024-11-26 15:46:00 +0530

    New Calculator program

---
+++ calculater.py
@@ -0,0 +1,8 @@
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+
+
+print("The value of", a, "+",b, "is: ", a + b)
+print("The value of", a, "-",b, "is: ", a - b)
+print("The value of", a, "*",b, "is: ", a * b)
+print("The value of", a, "/",b, "is: ", a / b)
